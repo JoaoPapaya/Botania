72f524bfd76b6c8d0cc6833bb68ff3865401c08b
João Mendonça <jbarbozamendonca@gmail.com>
2024-05-29 21:37:54 -0300

    executavel
    executavel

--- Assets/Scenes/Scripts/FallingObject.cs
+++ Assets/Scenes/Scripts/FallingObject.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Timeline;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.Events;
 
 
 public class FallingObject : MonoBehaviour
